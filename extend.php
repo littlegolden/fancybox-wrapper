bf444b074199c1a75a92940ebd14f4a6d3274bd2
Mark Cutting <2557946+phenomlab@users.noreply.github.com>
2021-02-05 16:12:51 +0000

    Update JS and CSS
    Changes made to increase performance

--- extend.php
+++ extend.php
@@ -16,9 +16,8 @@ use Flarum\Frontend\Document;
 return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
-            $document->head[] = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
-            $document->head[] = '<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>';
-            $document->head[] = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css">';
+            $document->head[] = '<script defer type="text/javascript" src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>';
+            $document->head[] = '<link rel="preload" as="style" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" onload="this.onload=null;this.rel=\'stylesheet\'">';
             $document->foot[] = <<<HTML
 <script>
  flarum.core.compat.extend.extend(flarum.core.compat['components/CommentPost'].prototype, 'oncreate', function(output, vnode) {
